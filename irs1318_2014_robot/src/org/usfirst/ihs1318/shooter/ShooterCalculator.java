7374d998f8df1aec092efa7d0f1d6ebe411a2600
b-megche <b-megche@B-MEGCHE.redmond.corp.microsoft.com>
2014-02-03 17:57:21 -0800

    Updated ShooterCalculator, ShooterRunner, UserInputData, ButtonRef

--- irs1318_2014_robot/src/org/usfirst/ihs1318/shooter/ShooterCalculator.java
+++ irs1318_2014_robot/src/org/usfirst/ihs1318/shooter/ShooterCalculator.java
@@ -15,135 +15,37 @@ public class ShooterCalculator {
 			ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(ShooterRef.EXTEND);
 		}
 		
+		if(ReferenceData.getInstance().getPressureSensorData().getIsPressurized() && ReferenceData.getInstance().getUserInputData().getRetractShooter()) {
+			ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(ShooterRef.RETRACT);
+			ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(ShooterRef.RETRACT);
+			ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(ShooterRef.RETRACT);
+		}
+		
+		if(ReferenceData.getInstance().getPressureSensorData().getIsPressurized() && ReferenceData.getInstance().getUserInputData().getExtendInnerShooterSolenoids()) {
+			ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(ShooterRef.RETRACT);
+			ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(ShooterRef.EXTEND);
+			ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(ShooterRef.RETRACT);
+		}
+		
+		if(ReferenceData.getInstance().getPressureSensorData().getIsPressurized() && ReferenceData.getInstance().getUserInputData().getExtendInnerThreeShooterSolenoids()) {
+			ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(ShooterRef.EXTEND);
+			ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(ShooterRef.EXTEND);
+			ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(ShooterRef.RETRACT); 
+		}
+		
+		if(ReferenceData.getInstance().getPressureSensorData().getIsPressurized() && ReferenceData.getInstance().getUserInputData().getExtendMiddleShooterSolenoid()) {
+			ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(ShooterRef.EXTEND);
+			ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(ShooterRef.RETRACT);
+			ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(ShooterRef.RETRACT); 
+		}
+		if(ReferenceData.getInstance().getPressureSensorData().getIsPressurized() && ReferenceData.getInstance().getUserInputData().getExtendShooterAngle()) {
+			ReferenceData.getInstance().getShooterData().setShooterAngleSolenoidState(ShooterRef.EXTEND);
+		}
+		if(ReferenceData.getInstance().getPressureSensorData().getIsPressurized() && ReferenceData.getInstance().getUserInputData().getRetractShooterAngle()) {
+			ReferenceData.getInstance().getShooterData().setShooterAngleSolenoidState(ShooterRef.RETRACT);
+		}
+		
 	}
-	//for the one piston extension macro
-		public void extendMiddlePiston() {
-			
-			if(ReferenceData.getInstance().getShooterData().getMiddleSolenoidState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(true);
-			}
-			
-		}
-		
-		//for the one piston retraction macro
-		public void retractMiddlePiston() {
-
-				
-			if(ReferenceData.getInstance().getShooterData().getMiddleSolenoidState() == true)
-			{
-					ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(false);
-			}
-				
-		}
-		
-		
-		//possible use for the two piston extension macro
-		//TODO decide to use inner or outer pistons for the two piston extension macro
-		public void extendInnerPistons() {
-			
-			if(ReferenceData.getInstance().getShooterData().getInnerSolenoidsState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(true);
-			}
-		}
-		
-		//possible use for the two piston retraction macro
-		public void retractInnerPistons() {
-				
-			if(ReferenceData.getInstance().getShooterData().getInnerSolenoidsState() == true)
-			{
-					ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(false);
-			}
-		}
-			
-		
-		//possible use for the two piston extension macro
-		//TODO decide to use inner or outer pistons for the two piston extension macro
-		public void extendOuterPistons() {
-			
-			if(ReferenceData.getInstance().getShooterData().getOuterSolenoidsState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(true);
-			}
-		}
-		
-		//possible use for the two piston retraction macro
-		public void retractOuterPistons() {
-			
-			if(ReferenceData.getInstance().getShooterData().getOuterSolenoidsState() == true)
-			{
-				ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(false);
-			}
-		}
-		
-		
-		//for the three piston extension macro
-		public void extendOuterAndMiddlePistons() {
-			
-			if(ReferenceData.getInstance().getShooterData().getOuterSolenoidsState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(true);
-			}
-			
-			if(ReferenceData.getInstance().getShooterData().getMiddleSolenoidState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(true);
-			}
-		}
-		
-		//for the three piston retraction macro
-		public void retractOuterAndMiddlePistons() {
-				
-			if(ReferenceData.getInstance().getShooterData().getOuterSolenoidsState() == true)
-			{
-					ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(false);
-			}
-				
-			if(ReferenceData.getInstance().getShooterData().getMiddleSolenoidState() == true)
-			{
-					ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(false);
-			}
-		}
-
-		
-		//for the piston extension macro
-		public void extendAllPistons() {
-			
-			if(ReferenceData.getInstance().getShooterData().getOuterSolenoidsState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(true);
-			}
-			
-			if(ReferenceData.getInstance().getShooterData().getInnerSolenoidsState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(true);
-			}
-			
-			if(ReferenceData.getInstance().getShooterData().getMiddleSolenoidState() == false)
-			{
-				ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(true);
-			}
-		}
-		
-		//for the five piston retraction macro
-		public void retractAllPistons() {
-				
-			if(ReferenceData.getInstance().getShooterData().getOuterSolenoidsState() == true)
-			{
-					ReferenceData.getInstance().getShooterData().setOuterSolenoidsState(false);
-			}
-				
-			if(ReferenceData.getInstance().getShooterData().getInnerSolenoidsState() == true)
-			{
-					ReferenceData.getInstance().getShooterData().setInnerSolenoidsState(false);
-			}
-				
-			if(ReferenceData.getInstance().getShooterData().getMiddleSolenoidState() == true)
-			{
-					ReferenceData.getInstance().getShooterData().setMiddleSolenoidState(false);
-			}
-		}
 		
 		
 }
