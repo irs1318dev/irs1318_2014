b6e3aaab8ba405f0c9c042c0b47b19be753b3c91
grant vaska <grantsansible@gmail.com>
2014-02-07 16:51:39 -0800

    Dipswitch stuff for compressor and pressure sensor.

--- irs1318_2014_robot/src/irs2014/shooter/PressureSensorReader.java
+++ irs1318_2014_robot/src/irs2014/shooter/PressureSensorReader.java
@@ -4,6 +4,8 @@ package irs2014.shooter;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import irs2014.components.RobotComponentBase;
+import irs2014.dipSwitch.DipSwitchData;
+import irs2014.dipSwitch.DipSwitchRef;
 import irs2014.generalData.PortRef;
 import irs2014.generalData.ReferenceData;
 
@@ -12,7 +14,11 @@ public class PressureSensorReader extends RobotComponentBase{
 	DigitalInput pressureSensor;
 	
 	public void robotInit() {
-		pressureSensor = new DigitalInput(PortRef.COMPETITION_PRESSURE_SENSOR);
+		if (ReferenceData.getInstance().getDipSwitchData().getDipSwitchState() == DipSwitchRef.COMPETITION_BOT){
+			pressureSensor = new DigitalInput(PortRef.COMPETITION_PRESSURE_SENSOR);
+		}else if (ReferenceData.getInstance().getDipSwitchData().getDipSwitchState() == DipSwitchRef.PRACTICE_BOT){
+			pressureSensor = new DigitalInput(PortRef.PRACTICE_PRESSURE_SENSOR);
+		}
 		System.out.println("PressureSensor robotInit()");
 	}
 	
