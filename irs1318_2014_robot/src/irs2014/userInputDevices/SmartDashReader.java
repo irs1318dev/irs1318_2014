285e4bcd4c8377061e534156b62487a45400e9bf
IRS1318 <IRS1318@DriveStation.mshome.net>
2014-03-03 21:45:15 -0800

    Working on smart dash stuff

--- irs1318_2014_robot/src/irs2014/userInputDevices/SmartDashReader.java
+++ irs1318_2014_robot/src/irs2014/userInputDevices/SmartDashReader.java
@@ -1,9 +1,22 @@
 package irs2014.userInputDevices;
-
 import irs2014.components.RobotComponentBase;
+import irs2014.generalData.ReferenceData;
+import irs2014.networkTable.IRSTable;
 
-public class SmartDashReader extends RobotComponentBase{
+public class SmartDashReader extends RobotComponentBase {
+	public void robotInit() {
+		IRSTable.putNumber(SmartDashRef.SHIFT_WAIT_TIME_INPUT, new Double(0));
+		IRSTable.putNumber(SmartDashRef.DISTANCE_INPUT, new Double(0));
+		IRSTable.putNumber(SmartDashRef.PAUSE_AFTER_DRIVING_INPUT, new Double(0));
+		IRSTable.putNumber(SmartDashRef.PAUSE_AFTER_SHOT_1_INPUT, new Double(0));
+		IRSTable.putNumber(SmartDashRef.COLLECTOR_IN_INPUT, new Double(0));
+		IRSTable.putNumber(SmartDashRef.PAUSE_AFTER_COLLECTOR_IN_INPUT, new Double(0));
+		IRSTable.putNumber(SmartDashRef.SPEED_INPUT, new Double(0));
+	}
 	
-	
-
+	public void teleopPeriodic() {
+		try {
+			ReferenceData.getInstance().getAutonomousVariableData()
+		}
+	}
 }
