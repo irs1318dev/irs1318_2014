a66e0f1fa0cc33138ea0b373d003be695dfa064b
Rainier Feiler <pharphuf7nik@gmail.com>
2014-03-04 15:56:46 -0800

    Autonomous variables now settable from SmartDash maybe I think

--- irs1318_2014_robot/src/irs2014/autonomous/tasks/AutoOpenDriveShootTwo.java
+++ irs1318_2014_robot/src/irs2014/autonomous/tasks/AutoOpenDriveShootTwo.java
@@ -1,6 +1,7 @@
 package irs2014.autonomous.tasks;
 
 import irs2014.autonomous.AutonomousCommand;
+import irs2014.generalData.ReferenceData;
 
 public class AutoOpenDriveShootTwo extends AutonomousCommand
 {
@@ -15,27 +16,27 @@ public class AutoOpenDriveShootTwo extends AutonomousCommand
 			extendShooterFrame();
 			break;
 		case 2:
-			pause(SHIFT_WAIT_TIME);
+			pause(ReferenceData.getInstance().getAutonomousVariableData().getShiftWaitTime());
 			break;
 		case 3:
 			collectorMotorIn(10000);// in milliseconds
-			goForwardRel(15 * 12 * 2.54, .7);//ft to inches to centimeters
+			goForwardRel(ReferenceData.getInstance().getAutonomousVariableData().getDistance() * 2.54, ReferenceData.getInstance().getAutonomousVariableData().getSpeed());//inches to centimeters
 			break;
 		case 4:
-			pause(1000);
+			pause(ReferenceData.getInstance().getAutonomousVariableData().getPauseAfterDriving());
 			break;
 		case 5:
 			System.out.println("We are launching ball 1!");
 			launch4Pistons();
 			break;
 		case 6:
-			pause(1200);
+			pause(ReferenceData.getInstance().getAutonomousVariableData().getPauseAfterShot1());
 			break;
 		case 7:
-			collectorMotorIn(1500);
+			collectorMotorIn(ReferenceData.getInstance().getAutonomousVariableData().getCollectorIn());
 			break;
 		case 8:
-			pause(600);
+			pause(ReferenceData.getInstance().getAutonomousVariableData().getPauseAfterCollectorIn());
 			break;
 		case 9:
 			launch5Pistons();
